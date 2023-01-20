ad6c26145ed48933211e9bdf3f0f2e31e6149364
kennedybabu <kennedynjogu8@gmail.com>
2023-01-20 10:08:00 +0300

    fix: fix countrypage breaking down on load

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCountry } from '../redux/features/countrysSlice'
 
@@ -13,25 +13,37 @@ const CountryPage = ({modifiedCountrys}) => {
 
   useEffect(() => {
     dispatch(fetchCountry({name}))
-  }, [])
+  }, [name]) 
 
 
   // useEffect(() => {
   //   if(country){
-  //     return {
-  //         name: country.name.common,
-  //         flag: country.flags.png,
-  //         population: country.population,
-  //         capitalCity: country.capital,
-  //         region: country.region 
+  //     return{
+  //       name: country[0].name.common,
+  //       population: country[0].population
   //     }
-  //   } 
+  //   }
   // }, [])
-  console.log(country)
 
   return (
-    <div className='w-full h-full p-2'>            
-        my country is {name}
+    <div className='w-full country-page p-2'>   
+        <Link to='/'>back</Link>         
+         {/* <div className='w-[250px] rounded-md h-[300px] my-4 mx-auto shadow'>
+          <img src={country[0].flags.png} alt={name} className='h-[45%] rounded-t-md w-full'/>
+          <div className='w-full h-full px-2 py-4'>
+            <h2 className='font-bold text-xl'>{name}</h2>
+            <p><span>Population</span>: {country[0].population}</p>
+            <p>Region: {country[0].region}</p>
+            <small><span className='font-bold'>Capital</span>: {country[0].capital}</small>
+          </div>
+      </div> */}
+      <img src={country[0]?.['flags']?.['png']} alt={name} className='h-[45%] rounded-t-md w-full'/>
+      {/* <p>{country[0].name.common}</p>
+      <p>{country[0].capital}</p>
+      <p>{country[0].population}</p> */}
+      <p>{country[0]?.['name']?.['common']}</p>
+      <p>{country[0]?.['capital']}</p>
+      <p>{country[0]?.['population']}</p>
     </div>
   )
 }
